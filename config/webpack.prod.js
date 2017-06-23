afb64b3c4bd6a9329a3b6cf2d8b1d6465d66c4ce
Marko Stijak <mstijak@gmail.com>
2017-06-23 11:29:36 +0200

    ...

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,10 +2,11 @@ var webpack = require('webpack'),
     ExtractTextPlugin = require("extract-text-webpack-plugin"),
     CopyWebpackPlugin = require('copy-webpack-plugin'),
     SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin'),
+    StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin'),
     BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
     merge = require('webpack-merge'),
     common = require('./webpack.config'),
-    path = require('path')
+    path = require('path');
 
 var sass = new ExtractTextPlugin({
     filename: "app.css",
@@ -27,10 +28,15 @@ var specific = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
-        new SWPrecacheWebpackPlugin(),
+        new SWPrecacheWebpackPlugin({
+            staticFileGlobsIgnorePatterns: [/_redirects/]
+        }),
         new webpack.optimize.ModuleConcatenationPlugin(),
         new webpack.optimize.UglifyJsPlugin(),
         sass,
+        new StyleExtHtmlWebpackPlugin({
+            minify: true
+        }),
         new CopyWebpackPlugin([{
             from: path.join(__dirname, '../assets'),
             to: path.join(__dirname, '../dist/assets'),
