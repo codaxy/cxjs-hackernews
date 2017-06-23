0b74265bc55ac0a5fc255bdada84b39626e0e59f
Marko Stijak <mstijak@gmail.com>
2017-06-23 15:36:29 +0200

    Preload instead of prefetch

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
         // }),
         new ScriptExtHtmlWebpackPlugin({
             async: /\.js$/,
-            prefetch: {
+            preload: {
                 test: /\.js$/,
                 chunks: 'async'
             }
