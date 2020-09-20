4987fe32d2e80f144988a2257298cc6d0f897a6d
Marko Stijak <mstijak@gmail.com>
2020-09-20 11:13:59 +0200

    Faster loading of next pages

--- app/routes/default/Controller.js
+++ app/routes/default/Controller.js
@@ -7,58 +7,60 @@ export default class extends Controller {
 	onInit() {
 		let now = Date.now();
 		//bust cache every 10 minutes
-		if (!this.store.get('cached') || now - this.store.get('cached') > 10 * 60 * 1000) {
+		if (
+			!this.store.get("cached") ||
+			now - this.store.get("cached") > 10 * 60 * 1000
+		) {
 			this.store.set("stories", []);
 			this.store.set("page", 0);
 			this.store.set("nextPage", 1);
-			this.store.set('cached', now);
+			this.store.set("cached", now);
 		}
 		this.scrollToTop();
-		this.addTrigger('load', ['nextPage'], ::this.load, true);
+		this.addTrigger("load", ["nextPage"], ::this.load, true);
 	}
 
 	scrollToTop() {
 		let scrollEl = document.scrollingElement || document.documentElement;
 
-		if (scrollState && scrollState.url == this.store.get('$root.url'))
+		if (scrollState && scrollState.url == this.store.get("$root.url"))
 			scrollEl.scrollTop = scrollState.top;
-		else
-			scrollEl.scrollTop = 0;
+		else scrollEl.scrollTop = 0;
 	}
 
 	saveScrollState() {
 		let scrollEl = document.scrollingElement || document.documentElement;
 		scrollState = {
 			top: scrollEl.scrollTop,
-			url: this.store.get('$root.url')
-		}
+			url: this.store.get("$root.url"),
+		};
 	}
 
 	reload(e) {
-		if (e)
-			e.preventDefault();
+		if (e) e.preventDefault();
 
 		this.load();
 	}
 
 	load() {
 		let channel = this.store.get("$root.url").substring(2) || "news";
-		let page = this.store.get('nextPage');
-		if (page == this.store.get('page'))
-			return;
+		let page = this.store.get("nextPage");
+		if (page == this.store.get("page")) return;
 
-		if (this.store.get("status") != 'ok')
-			this.store.set("status", "loading");
+		if (this.store.get("status") != "ok") this.store.set("status", "loading");
 
 		fetchStories(channel, page)
-			.then(moreStories => {
-				this.store.update('stories', stories => [...stories, ...moreStories]);
+			.then((moreStories) => {
+				this.store.update("stories", (stories) => [
+					...stories,
+					...moreStories,
+				]);
 				this.store.set("status", "ok");
-				this.store.set('page', page);
+				this.store.set("page", page);
 			})
-			.catch(e => {
+			.catch((e) => {
 				console.error(e);
-				if (this.store.get("status") != 'ok')
+				if (this.store.get("status") != "ok")
 					this.store.set("status", "error");
 				else {
 					//display an error overlay?
@@ -67,9 +69,9 @@ export default class extends Controller {
 	}
 
 	loadMore(depth) {
-		let status = this.store.get("status");
+		//let status = this.store.get("status");
 		let stories = this.store.get("stories");
-		if (stories.length > 0 && depth < 1000)
-			this.store.set('nextPage', this.store.get('page') + 1);
+		if (stories.length > 0 && depth < 2000)
+			this.store.set("nextPage", this.store.get("page") + 1);
 	}
 }
